be69412dae3cfbd9f47e7bd9556928a059db22ac
iltaek <iltaekkwon@gmail.com>
2019-12-08 20:05:41 +0900

    feat(front): 게시글 수정 화면 작성

--- src/main/java/com/iltaek/example/springboot/web/IndexController.java
+++ src/main/java/com/iltaek/example/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.iltaek.example.springboot.web;
 
+import com.iltaek.example.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import com.iltaek.example.springboot.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by iltaek on 2019/12/08 Blog : http://blog.iltaek.me Github : http://github.com/iltaek
@@ -24,5 +26,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
