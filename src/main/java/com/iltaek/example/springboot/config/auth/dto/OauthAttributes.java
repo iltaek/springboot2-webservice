040b572356914336280e918b05b9a9a5ed177f84
iltaek <iltaekkwon@gmail.com>
2019-12-15 23:20:32 +0900

    feat(sec): oauth2(네이버) 로그인 기능 구현

--- src/main/java/com/iltaek/example/springboot/config/auth/dto/OauthAttributes.java
+++ src/main/java/com/iltaek/example/springboot/config/auth/dto/OauthAttributes.java
@@ -28,7 +28,22 @@ public class OauthAttributes {
     }
 
     public static OauthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OauthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OauthAttributes.builder()
+            .name((String) response.get("name"))
+            .email((String) response.get("email"))
+            .picture((String) attributes.get("picture"))
+            .attributes(response)
+            .nameAttributeKey(userNameAttributeName)
+            .build();
     }
 
     private static OauthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
